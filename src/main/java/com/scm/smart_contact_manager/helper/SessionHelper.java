caf0acfcde7c129c3fcc03c8f7408b88c0ef434c
Deepak Sharma <dsharma2828@gmail.com>
2024-10-25 19:20:07 +0530

    fixed issue of message removing

--- src/main/java/com/scm/smart_contact_manager/helper/SessionHelper.java
+++ src/main/java/com/scm/smart_contact_manager/helper/SessionHelper.java
@@ -1,15 +1,26 @@
-//package com.scm.smart_contact_manager.helper;
-//
-//import jakarta.servlet.http.HttpSession;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-//@Component
-//public class SessionHelper {
-//    public static void removeMessage() {
-//        // remove message from session
-//        HttpSession session = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes()
-//                .g
-//    }
-//}
+package com.scm.smart_contact_manager.helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Objects;
+
+@Component
+public class SessionHelper {
+
+    public void removeMessage() {
+        try {
+            System.out.println("removing message from session");
+            HttpSession session = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest()
+                    .getSession();
+            session.removeAttribute("message");
+        } catch (Exception e) {
+            System.out.println("Error in SessionHelper: " + e.getMessage());
+        }
+
+    }
+
+}
